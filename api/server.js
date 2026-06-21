2e33bb9f86829d6fc143d4bc38449b5c35ca57df
Liam Friedman <liamfriedman@Liams-MacBook-Air.local>
2026-06-21 17:07:52 +0300

    fix static path to public folder

--- api/server.js
+++ api/server.js
@@ -14,7 +14,7 @@ const DATABASE_URL = process.env.DATABASE_URL;
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static(__dirname));
+app.use(express.static(path.join(__dirname, '../public')));
 
 function loadData() {
   try {
